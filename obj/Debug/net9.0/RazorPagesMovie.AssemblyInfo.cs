dd3d1ca4efbddc4fcf3d2f6b58d54c7f390fc925
10Ngwako <ngoakoramakgoakgoa@gmail.com>
2025-10-22 11:05:00 +0200

    updating the pages

--- obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995d33930baed4db4a3879d79cd0a7030f419b15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8fb28d1b56ac3a7764f89c754114a7d127b3aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
